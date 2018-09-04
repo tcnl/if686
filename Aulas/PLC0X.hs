77b126a025997525d93251a6d27d63d78b3baa2f
Thiago Casa Nova da Luz <tcnl@ug1c33.windows.cin.ufpe.br>
2018-09-04 14:55:56 -0300

    exercicios feitos

--- Aulas/PLC0X.hs
+++ Aulas/PLC0X.hs
@@ -3,3 +3,24 @@
 somaLista :: [Int] -> Int
 somaLista [] = 0
 somaLista (x:xs) = x + somaLista xs
+
+double :: [Int] -> [Int]
+double [] = []
+double (x:xs) = (2 * x) : double xs
+
+member :: [Int] -> Int -> Bool
+member [] n = False
+member (x:xs) n = (x == n) || member xs n
+
+eDigito :: Char -> Bool
+eDigito ch = (ch >= '0') && (ch <= '9')
+
+digits :: [Char] -> [Char]
+digits [] = []
+digits (x:xs)
+    | eDigito x = x : digits xs
+    | otherwise = digits xs
+
+somaParesLista :: [(Int,Int)] -> [Int]
+somaParesLista [] = []
+somaParesLista ((a,b):xs) = (a+b) : somaParesLista xs 
